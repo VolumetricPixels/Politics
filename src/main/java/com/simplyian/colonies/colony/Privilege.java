13896f5f650caba417db7f666bb1a85707af4afe
Ian Macalinao <ianmacalinao@gmail.com>
2012-08-16 17:59:57 -0500

    Move Privilege to colony package
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/com/simplyian/colonies/colony/Privilege.java
+++ src/main/java/com/simplyian/colonies/colony/Privilege.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.simplyian.colonies.privilege;
+package com.simplyian.colonies.colony;
 
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
