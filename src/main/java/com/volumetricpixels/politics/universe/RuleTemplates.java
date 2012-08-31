b78224571b62c1a49a1b65e97f4c9b5e22aad92b
Ian Macalinao <ianmacalinao@gmail.com>
2012-08-31 07:28:02 -0500

    Added a method to copy rule templates to the rules folder. Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/com/volumetricpixels/politics/universe/RuleTemplates.java
+++ src/main/java/com/volumetricpixels/politics/universe/RuleTemplates.java
@@ -1,14 +1,19 @@
 package com.volumetricpixels.politics.universe;
 
+import com.volumetricpixels.politics.Politics;
 import com.volumetricpixels.politics.PoliticsPlugin;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.security.CodeSource;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  * Rule template helper class.
@@ -18,6 +23,33 @@ public final class RuleTemplates {
 	 * C'tor
 	 */
 	private RuleTemplates() {
+	}
+
+	/**
+	 * Copies a template with the given name to a new ruleset with the given
+	 * name.
+	 *
+	 * @param name
+	 * @param as
+	 */
+	public static void copyTemplate(String name, String as) {
+		InputStream templateStream = RuleTemplates.class.getResourceAsStream("templates/" + name.toLowerCase() + ".yml");
+		if (templateStream == null) {
+			throw new IllegalArgumentException("Template does not exist!");
+		}
+		StringWriter writer = new StringWriter();
+		try {
+			IOUtils.copy(templateStream, writer, Charset.defaultCharset());
+		} catch (IOException ex) {
+			PoliticsPlugin.logger().log(Level.SEVERE, "Could not read template " + name + "!", ex);
+		}
+
+		File dest = new File(Politics.getPlugin().getDataFolder(), "templates/" + as + ".yml");
+		try {
+			FileUtils.writeStringToFile(dest, writer.toString());
+		} catch (IOException ex) {
+			PoliticsPlugin.logger().log(Level.SEVERE, "Could not write template as " + dest.getPath() + "!", ex);
+		}
 	}
 
 	/**
@@ -38,8 +70,8 @@ public final class RuleTemplates {
 
 				while ((ze = zip.getNextEntry()) != null) {
 					String entryName = ze.getName();
-					if (entryName.startsWith("templates")) {
-						set.add(entryName);
+					if (entryName.startsWith("templates") && entryName.endsWith(".yml")) {
+						set.add(entryName.substring("templates".length(), entryName.length() - 4));
 					}
 				}
 
