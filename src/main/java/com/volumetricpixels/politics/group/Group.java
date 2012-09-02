cb797ce73bab4eed0463f0842c73445dd414df93
Ian Macalinao <ianmacalinao@gmail.com>
2012-09-02 13:50:16 -0500

    This is not optimal @DziNeIT, check my comments on your commit Revert "Better getIntProperty method"
    This reverts commit 44de2de566266a409b355765a70b94f232018349.

--- src/main/java/com/volumetricpixels/politics/group/Group.java
+++ src/main/java/com/volumetricpixels/politics/group/Group.java
@@ -223,11 +223,15 @@ public final class Group implements Comparable<Group>, Storable {
      * @return
      */
     public int getIntProperty(int property, int def) {
-        try {
-            return Integer.parseInt(getStringProperty(property));
-        } catch (NumberFormatException e) {
-            return def;
+        Object p = getProperty(property);
+        if (p != null) {
+            if (p instanceof Integer) {
+                return ((Integer) p).intValue();
+            } else {
+                return def;
+            }
         }
+        return def;
     }
 
     /**
