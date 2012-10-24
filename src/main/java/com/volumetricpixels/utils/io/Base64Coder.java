c8d8c206bbe94e684367782549660fdc7aad36a4
Ollie <dzineit@live.co.uk>
2012-10-24 22:09:41 +0100

    Remove Politics headers from VP util files... Again... *Cough Ian Cough*
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/main/java/com/volumetricpixels/utils/io/Base64Coder.java
+++ src/main/java/com/volumetricpixels/utils/io/Base64Coder.java
@@ -1,22 +1,3 @@
-/*
- * This file is part of Politics.
- *
- * Copyright (c) 2012-2012, VolumetricPixels <http://volumetricpixels.com/>
- * Politics is licensed under the Affero General Public License Version 3.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.volumetricpixels.utils.io;
 
 public class Base64Coder {
@@ -53,28 +34,28 @@ public class Base64Coder {
         }
     }
 
-    public static String encodeString(String s) {
+    public static String encodeString(final String s) {
         return new String(encode(s.getBytes()));
     }
 
-    public static String encodeLines(byte[] in) {
+    public static String encodeLines(final byte[] in) {
         return encodeLines(in, 0, in.length, 76, systemLineSeparator);
     }
 
-    public static String encodeLines(byte[] in, int iOff, int iLen, int lineLen, String lineSeparator) {
-        int blockLen = (lineLen * 3) / 4;
+    public static String encodeLines(final byte[] in, final int iOff, final int iLen, final int lineLen, final String lineSeparator) {
+        final int blockLen = lineLen * 3 / 4;
 
         if (blockLen <= 0) {
             throw new IllegalArgumentException();
         }
 
-        int lines = (iLen + blockLen - 1) / blockLen;
-        int bufLen = ((iLen + 2) / 3) * 4 + lines * lineSeparator.length();
+        final int lines = (iLen + blockLen - 1) / blockLen;
+        final int bufLen = (iLen + 2) / 3 * 4 + lines * lineSeparator.length();
 
-        StringBuilder buf = new StringBuilder(bufLen);
+        final StringBuilder buf = new StringBuilder(bufLen);
         int ip = 0;
         while (ip < iLen) {
-            int l = Math.min(iLen - ip, blockLen);
+            final int l = Math.min(iLen - ip, blockLen);
             buf.append(encode(in, iOff + ip, l));
             buf.append(lineSeparator);
             ip += l;
@@ -83,30 +64,31 @@ public class Base64Coder {
         return buf.toString();
     }
 
-    public static char[] encode(byte[] in) {
+    public static char[] encode(final byte[] in) {
         return encode(in, 0, in.length);
     }
 
-    public static char[] encode(byte[] in, int iLen) {
+    public static char[] encode(final byte[] in, final int iLen) {
         return encode(in, 0, iLen);
     }
 
-    public static char[] encode(byte[] in, int iOff, int iLen) {
-        int oDataLen = (iLen * 4 + 2) / 3; // output length without padding
-        int oLen = ((iLen + 2) / 3) * 4; // output length including padding
-        char[] out = new char[oLen];
+    public static char[] encode(final byte[] in, final int iOff, final int iLen) {
+        final int oDataLen = (iLen * 4 + 2) / 3; // output length without
+                                                 // padding
+        final int oLen = (iLen + 2) / 3 * 4; // output length including padding
+        final char[] out = new char[oLen];
         int ip = iOff;
-        int iEnd = iOff + iLen;
+        final int iEnd = iOff + iLen;
         int op = 0;
 
         while (ip < iEnd) {
-            int i0 = in[ip++] & 0xff;
-            int i1 = ip < iEnd ? in[ip++] & 0xff : 0;
-            int i2 = ip < iEnd ? in[ip++] & 0xff : 0;
-            int o0 = i0 >>> 2;
-            int o1 = ((i0 & 3) << 4) | (i1 >>> 4);
-            int o2 = ((i1 & 0xf) << 2) | (i2 >>> 6);
-            int o3 = i2 & 0x3F;
+            final int i0 = in[ip++] & 0xff;
+            final int i1 = ip < iEnd ? in[ip++] & 0xff : 0;
+            final int i2 = ip < iEnd ? in[ip++] & 0xff : 0;
+            final int o0 = i0 >>> 2;
+            final int o1 = (i0 & 3) << 4 | i1 >>> 4;
+            final int o2 = (i1 & 0xf) << 2 | i2 >>> 6;
+            final int o3 = i2 & 0x3F;
             out[op++] = map1[o0];
             out[op++] = map1[o1];
             out[op] = op < oDataLen ? map1[o2] : '=';
@@ -118,15 +100,15 @@ public class Base64Coder {
         return out;
     }
 
-    public static String decodeString(String s) {
+    public static String decodeString(final String s) {
         return new String(decode(s));
     }
 
-    public static byte[] decodeLines(String s) {
-        char[] buf = new char[s.length()];
+    public static byte[] decodeLines(final String s) {
+        final char[] buf = new char[s.length()];
         int p = 0;
         for (int ip = 0; ip < s.length(); ip++) {
-            char c = s.charAt(ip);
+            final char c = s.charAt(ip);
             if (c != ' ' && c != '\r' && c != '\n' && c != '\t') {
                 buf[p++] = c;
             }
@@ -135,15 +117,15 @@ public class Base64Coder {
         return decode(buf, 0, p);
     }
 
-    public static byte[] decode(String s) {
+    public static byte[] decode(final String s) {
         return decode(s.toCharArray());
     }
 
-    public static byte[] decode(char[] in) {
+    public static byte[] decode(final char[] in) {
         return decode(in, 0, in.length);
     }
 
-    public static byte[] decode(char[] in, int iOff, int iLen) {
+    public static byte[] decode(final char[] in, final int iOff, int iLen) {
         if (iLen % 4 != 0) {
             throw new IllegalArgumentException("Length of Base64 encoded input string is not a multiple of 4.");
         }
@@ -152,34 +134,34 @@ public class Base64Coder {
             iLen--;
         }
 
-        int oLen = (iLen * 3) / 4;
-        byte[] out = new byte[oLen];
+        final int oLen = iLen * 3 / 4;
+        final byte[] out = new byte[oLen];
         int ip = iOff;
-        int iEnd = iOff + iLen;
+        final int iEnd = iOff + iLen;
         int op = 0;
 
         while (ip < iEnd) {
-            int i0 = in[ip++];
-            int i1 = in[ip++];
-            int i2 = ip < iEnd ? in[ip++] : 'A';
-            int i3 = ip < iEnd ? in[ip++] : 'A';
+            final int i0 = in[ip++];
+            final int i1 = in[ip++];
+            final int i2 = ip < iEnd ? in[ip++] : 'A';
+            final int i3 = ip < iEnd ? in[ip++] : 'A';
 
             if (i0 > 127 || i1 > 127 || i2 > 127 || i3 > 127) {
                 throw new IllegalArgumentException("Illegal character in Base64 encoded data.");
             }
 
-            int b0 = map2[i0];
-            int b1 = map2[i1];
-            int b2 = map2[i2];
-            int b3 = map2[i3];
+            final int b0 = map2[i0];
+            final int b1 = map2[i1];
+            final int b2 = map2[i2];
+            final int b3 = map2[i3];
 
             if (b0 < 0 || b1 < 0 || b2 < 0 || b3 < 0) {
                 throw new IllegalArgumentException("Illegal character in Base64 encoded data.");
             }
 
-            int o0 = (b0 << 2) | (b1 >>> 4);
-            int o1 = ((b1 & 0xf) << 4) | (b2 >>> 2);
-            int o2 = ((b2 & 3) << 6) | b3;
+            final int o0 = b0 << 2 | b1 >>> 4;
+            final int o1 = (b1 & 0xf) << 4 | b2 >>> 2;
+            final int o2 = (b2 & 3) << 6 | b3;
             out[op++] = (byte) o0;
 
             if (op < oLen) {
