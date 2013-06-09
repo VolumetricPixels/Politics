28f7f9a5565de5d232f07769807278d84e165468
DziNeIT <dzineit@live.co.uk>
2013-06-09 19:02:11 +0100

    Merge volumetricpixels/utils package with politics/util. Move Serialization's methods to PropertySerializer
    Signed-off-by: DziNeIT <dzineit@live.co.uk>

--- src/main/java/com/volumetricpixels/politics/util/Base64Coder.java
+++ src/main/java/com/volumetricpixels/politics/util/Base64Coder.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.volumetricpixels.utils.io;
+package com.volumetricpixels.politics.util;
 
 public final class Base64Coder {
     // The line separator string of the operating system.
--- src/main/java/com/volumetricpixels/politics/util/PropertySerializer.java
+++ src/main/java/com/volumetricpixels/politics/util/PropertySerializer.java
@@ -19,20 +19,22 @@
  */
 package com.volumetricpixels.politics.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import com.volumetricpixels.politics.exception.PropertyDeserializationException;
 import com.volumetricpixels.politics.exception.PropertySerializationException;
 
-import com.volumetricpixels.utils.io.Serialization;
 
 /**
  * Contains various methods for serializing and deserializing certain properties
  * as strings
  */
 public final class PropertySerializer {
-
     /**
      * Serializes any serializable object to a Base64 encoded String
      * 
@@ -42,7 +44,7 @@ public final class PropertySerializer {
      */
     public static String serialize(final Serializable obj) throws PropertySerializationException {
         try {
-            return Serialization.toString(obj);
+            return PropertySerializer.toString(obj);
         } catch (final IOException e) {
             throw new PropertySerializationException("IOException occurred while serializing an object of type " + obj.getClass().getName() + "!", e);
         }
@@ -57,12 +59,66 @@ public final class PropertySerializer {
      */
     public static Object deserialize(final String string) throws PropertyDeserializationException {
         try {
-            return Serialization.fromString(string);
+            return PropertySerializer.fromString(string);
         } catch (final ClassNotFoundException e) {
             throw new PropertyDeserializationException("Could not find the object class for the given string while deserializing!", e);
         } catch (final IOException e) {
             throw new PropertyDeserializationException("IOException occurred while deserializing a string!", e);
         }
+    }
+
+    /**
+     * Converts a Serializable Object into a Serialized String that is encoded
+     * in Base64.
+     * 
+     * Can be used along with fromString(String) to store Objects as Strings and
+     * later get them back. Please note this can be used to save to Files as
+     * well, meaning it is a great way for storing Objects without loads of
+     * manual parsing and creation of objects.
+     * 
+     * @param o
+     *            The Serializable Object to stringify
+     * @return The Serialized String
+     * 
+     * @throws IOException
+     *             When there is a write error
+     */
+    private static String toString(final Serializable o) throws IOException {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(o);
+        oos.close();
+        byte[] byteArray = baos.toByteArray();
+        return new String(Base64Coder.encode(byteArray, 0, byteArray.length));
+    }
+
+    /**
+     * Uses Base64 along with Java's Serialization API to get an Object from a
+     * Serialized Base64 String.
+     * 
+     * Please note the input String should be encoded in Base64. If you use the
+     * toString(Serializable) method, the result will be encoded in Base64, so
+     * as long as you got the Serialized String via toString(Serializable) in
+     * the first place, everything should work.
+     * 
+     * You can cast the returned Object to whatever type the String was from in
+     * the first place.
+     * 
+     * @param s
+     *            The String to convert
+     * @return The Object the String was converted to
+     * 
+     * @throws IOException
+     *             If there is a read error
+     * @throws ClassNotFoundException
+     *             If the written Object's class is not found
+     */
+    private static Object fromString(final String s) throws IOException, ClassNotFoundException {
+        final byte[] data = Base64Coder.decode(s.toCharArray(), 0, s.length());
+        final ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+        final Object o = ois.readObject();
+        ois.close();
+        return o;
     }
 
     private PropertySerializer() {
